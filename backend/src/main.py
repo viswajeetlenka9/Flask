339a479aa519d2ca3d7403de8a80a4a8ff798fa8
Viswajeet Lenka <viswajeetlenka9@gmail.com>
2020-03-09 10:49:57 +0530

    CRUD operation Done

--- backend/src/main.py
+++ backend/src/main.py
@@ -3,9 +3,15 @@
 from flask import Flask, jsonify, request
 from .entities.entity import Session, engine, Base
 from .entities.exam import Exam, ExamSchema
+from flask_cors import CORS
 
 # creating the Flask application
 app = Flask(__name__)
+
+CORS(app)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=9010)
 
 # if needed, generate database schema
 Base.metadata.create_all(engine)
@@ -18,60 +24,65 @@ def index():
 
 @app.route('/exams')
 def get_exams():
-    # fetching from the database
     session = Session()
     exam_objects = session.query(Exam).all()
-
-    # transforming into JSON-serializable objects
     schema = ExamSchema(many=True)
     exams = schema.dump(exam_objects)
-
-    # serializing as JSON
     session.close()
-    return jsonify(results = exams)
+    response = jsonify(results = exams)
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
 
 
-@app.route('/exams', methods=['POST'])
+@app.route('/exams/addexam', methods=['POST'])
 def add_exam():
-    # mount exam object
     posted_exam = ExamSchema(only=('title', 'description'))\
         .load(request.get_json())
 
     exam = Exam(**posted_exam, created_by="HTTP post request")
-
-    # persist exam
     session = Session()
     session.add(exam)
     session.commit()
-
-    # return created exam
     new_exam = ExamSchema().dump(exam)
     session.close()
     return jsonify(new_exam), 201
-'''
-# start session
-session = Session()
 
-# check for existing data
-exams = session.query(Exam).all()
 
-if len(exams) == 0:
-    # create and persist dummy exam
-    python_exam = Exam("SQLAlchemy Exam", "Test your knowledge about SQLAlchemy.", "script")
-    session.add(python_exam)
-    session.commit()
+@app.route('/exams/deleteexam', methods=['POST'])
+def delete_exam():
+    title = request.args.get('title')
+    session = Session()
+    delete_object = session.query(Exam).filter_by(title = title).first()
+
+    if delete_object is not None:
+    	session.delete(delete_object)
+    	session.commit()
+    exam_objects = session.query(Exam).all()
+    schema = ExamSchema(many=True)
+    exams = schema.dump(exam_objects)
     session.close()
+    response = jsonify(results = exams)
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response,201
 
-    # reload exams
-    exams = session.query(Exam).all()
+@app.route('/exams/updateexam', methods=['POST'])
+def update_exam():
+    posted_exam = ExamSchema(only=('title', 'description'))\
+        .load(request.get_json())
+    exam = Exam(**posted_exam, created_by="HTTP post request")
+    title = request.args.get('title')
+    session = Session()
+    update_object = session.query(Exam).filter_by(title = title).first()
 
-# show existing exams
-print('### Exams:')
-for exam in exams:
-    print(f'({exam.id}) {exam.title} - {exam.description}')
-
-
-'''
-
-if __name__ == "__main__":
-	app.run(debug = True, port=9090)
+    if update_object is not None:
+    	update_object.title = exam.title
+    	update_object.description = exam.description
+    	session.add(update_object)
+    	session.commit()
+    exam_objects = session.query(Exam).all()
+    schema = ExamSchema(many=True)
+    exams = schema.dump(exam_objects)
+    session.close()
+    response = jsonify(results = exams)
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response,201
